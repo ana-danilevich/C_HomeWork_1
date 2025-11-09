149a930853a94a35628ddec9d5abe36ac65b7569
Anastasia <ana.danilevi4@gmail.com>
2025-11-09 17:17:46 +0300

    Добавила 5, 6 и 7 задания

---
+++ App_6/Task_6.cs
@@ -0,0 +1,7 @@
+﻿/*Создайте программу, которая запрашивает у пользователя число в виде строки. Преобразуйте его
+в тип int и выведите его квадрат на экран.*/
+Console.Write("Введите чисто: ");
+string userNumber = Console.ReadLine();
+int num = int.Parse(userNumber);
+int square = num * num;
+Console.WriteLine($"Квадрат числа {num} равен {square}");
---
+++ App_7/Task_7.cs
@@ -0,0 +1,7 @@
+﻿/*Напишите программу, которая запрашивает у пользователя дробное число, округляет его до
+ближайшего целого и выводит результат. Используйте методы класса Math для округления.*/
+Console.Write("Введите дробное число: ");
+string userNumber = Console.ReadLine();
+double num = double.Parse(userNumber);
+double roundedNumber = Math.Round(num);
+Console.WriteLine($"Число {num} округлилось до {roundedNumber}");
