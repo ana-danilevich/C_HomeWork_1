8b903d420c14499bbb4e7cccb9fdc346b0ce90f3
Anastasia <ana.danilevi4@gmail.com>
2025-11-09 17:15:37 +0300

    Добавила 1 задание

---
+++ FirstApp/Task_1.cs
@@ -0,0 +1,13 @@
+﻿/*Создайте программу, в которой объявите переменные разных типов данных (int, double, char,
+string, bool). Присвойте им значения и выведите их на экран.*/
+int intValue = 136548;
+double doubleValue = 1.7215;
+char charValue = 'L';
+string stringValue = "Hello";
+bool boolValue = false;
+
+Console.WriteLine("intValue: " + intValue);
+Console.WriteLine("doubleValue: " + doubleValue);
+Console.WriteLine("charValue: " + charValue);
+Console.WriteLine("stringValue: " + stringValue);
+Console.WriteLine("boolValue: " + boolValue);
